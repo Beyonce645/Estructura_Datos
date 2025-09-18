6845682b31ab006c595cb3fc5462d67bc2afa9e7
Beyonce645 <beyonce.ramon@edu.uah.es>
2025-09-18 20:22:46 +0200

    Ejemplo06.cpp

--- Ejemplo06.cpp
+++ Ejemplo06.cpp
@@ -23,4 +23,11 @@ int main (int argc, char** argv)
         
     };
     return 0;
+
 }
+/* Cuantos datos va a introducir? 3
+Introduzca el dato 1/3: 5
+Introduzca el dato 2/3: 7
+Introduzca el dato 3/3: 4
+La media de los 3 datos es 5.33333.
+*/
